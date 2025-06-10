fd543cd71aefa911748a81bb6b8707cf2a161a26
Luca Fondo <luca.fondo@trai.ch>
2025-06-10 12:44:01 +0200

    refactor rename from msgen to gecro

--- cmd/new.go
+++ cmd/new.go
@@ -18,7 +18,7 @@ var newCmd = &cobra.Command{
 	Use:   "new [servicename]",
 	Short: "Creates a new microservice",
 	Long: `Creates a new microservice with a specified name
-	Example: msgen new test`,
+	Example: gecro new test`,
 	Args: cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		const moduleFile = "MODULE.bazel"
